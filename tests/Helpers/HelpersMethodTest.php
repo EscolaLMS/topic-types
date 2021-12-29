cb4e6a4aae0be15c67393f04ff48c9a2a9978e84
Hubert Krzysztofiak <hubertkrzyszt@gmail.com>
2021-12-29 10:35:59 +0100

    Add test markdown helper

---
+++ tests/Helpers/HelpersMethodTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Helpers;
+
+use EscolaLms\Courses\Models\Topic;
+use EscolaLms\TopicTypes\Facades\Markdown;
+use EscolaLms\TopicTypes\Tests\TestCase;
+
+class HelpersMethodTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->topic = Topic::factory()->create();
+    }
+
+    public function testConvertImagesMethos()
+    {
+        $topic = $this->topic;
+        $course = $topic->lesson->course;
+        $destinationPrefix = sprintf('courses/%d/topic/%d/', $course->id, $topic->id);
+
+        $result = Markdown::convertImagesPathsForImageApi('lorem ipsum', $destinationPrefix);
+        $this->assertArrayHasKey('value', $result);
+        $this->assertTrue($result['value'] === 'lorem ipsum');
+    }
+}
