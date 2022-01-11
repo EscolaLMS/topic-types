bb77e2edf8c3d706e38b45fdb73aa0caa0bd91d0
Tomasz Smolarek <tomasz.smolarek@escolasoft.com>
2022-01-11 16:26:14 +0100

    fix asset path in scorm sco topic type

--- src/Models/TopicContent/ScormSco.php
+++ src/Models/TopicContent/ScormSco.php
@@ -2,7 +2,12 @@
 
 namespace EscolaLms\TopicTypes\Models\TopicContent;
 
+use EscolaLms\Scorm\Services\Contracts\ScormServiceContract;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use Peopleaps\Scorm\Model\ScormScoModel;
+use Symfony\Component\Finder\Exception\DirectoryNotFoundException;
 
 /**
  * @OA\Schema(
@@ -38,5 +43,39 @@ class ScormSco extends AbstractTopicContent
     protected static function newFactory()
     {
         return \EscolaLms\TopicTypes\Database\Factories\TopicContent\ScormScoFactory::new();
+    }
+
+
+    public function fixAssetPaths(): array
+    {
+        $topic = $this->topic;
+        $course = $topic->lesson->course;
+        $destination = sprintf('courses/%d/topic/%d/%s', $course->id, $topic->id, 'export.zip');
+
+        /** @var ScormServiceContract $service */
+        $service = app(ScormServiceContract::class);
+        $zipPath = $service->zipScorm(ScormScoModel::find($this->value)->scorm->getKey());
+
+        $disk = Storage::disk('local'); // this is always 'local' for scorm // TODO check
+        if ($disk->exists($destination)) {
+            $disk->delete($destination);
+        }
+        $destinationPath = $disk->path($destination);
+        $concurrentDirectory = dirname($destinationPath);
+
+        if (!mkdir($concurrentDirectory, 0777, true) && !is_dir($concurrentDirectory)) {
+            throw new DirectoryNotFoundException(
+                sprintf('Directory "%s" was not created', $concurrentDirectory)
+            );
+        }
+
+        copy($zipPath, $destinationPath);
+
+        return [[$zipPath, $destinationPath]];
+    }
+
+    public function getMorphClass()
+    {
+        return self::class;
     }
 }
