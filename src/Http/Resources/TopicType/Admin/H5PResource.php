72f2a4113b4b58a60e0ebc939dbabbd1c619e42e
Tomasz Smolarek <tomasz.smolarek@escolasoft.com>
2022-08-03 15:31:05 +0200

    fix

--- src/Http/Resources/TopicType/Admin/H5PResource.php
+++ src/Http/Resources/TopicType/Admin/H5PResource.php
@@ -4,6 +4,7 @@ namespace EscolaLms\TopicTypes\Http\Resources\TopicType\Admin;
 
 use EscolaLms\HeadlessH5P\Models\H5PContent;
 use EscolaLms\HeadlessH5P\Services\Contracts\HeadlessH5PServiceContract;
+use EscolaLms\HeadlessH5P\Services\HeadlessH5PService;
 use EscolaLms\TopicTypes\Http\Resources\TopicType\Contacts\TopicTypeResourceContract;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,7 +15,7 @@ class H5PResource extends JsonResource implements TopicTypeResourceContract
         return [
             'id' => $this->id,
             'value' => $this->value,
-            'content' => app(HeadlessH5PServiceContract::class)->getContentSettings($this->value),
+            'content' => $this->value ?? app(HeadlessH5PService::class)->getContentSettings($this->value),
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
