9460c377da3437a0593f0dde729230fc7f018621
Tomasz Smolarek <tomasz.smolarek@escolasoft.com>
2022-08-03 15:03:23 +0200

    update h5p resources

--- src/Http/Resources/TopicType/Client/H5PResource.php
+++ src/Http/Resources/TopicType/Client/H5PResource.php
@@ -3,6 +3,7 @@
 namespace EscolaLms\TopicTypes\Http\Resources\TopicType\Client;
 
 use EscolaLms\HeadlessH5P\Models\H5PContent;
+use EscolaLms\HeadlessH5P\Services\Contracts\HeadlessH5PServiceContract;
 use EscolaLms\TopicTypes\Http\Resources\TopicType\Contacts\TopicTypeResourceContract;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -13,7 +14,7 @@ class H5PResource extends JsonResource implements TopicTypeResourceContract
         return [
             'id' => $this->id,
             'value' => $this->value,
-            'content' => H5PContent::find($this->value),
+            'content' => app(HeadlessH5PServiceContract::class)->getContentSettings($this->value),
         ];
     }
 }
