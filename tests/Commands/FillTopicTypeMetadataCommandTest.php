a593de81a89c1d6873a74a8a63404346a4400733
Tomasz Smolarek <tomasz.smolarek@escolasoft.com>
2023-07-28 11:13:02 +0200

    fix tests

--- tests/Commands/FillTopicTypeMetadataCommandTest.php
+++ tests/Commands/FillTopicTypeMetadataCommandTest.php
@@ -84,6 +84,7 @@ class FillTopicTypeMetadataCommandTest extends TestCase
         $this->assertNotNull($topicable_video->length);
         $this->assertEquals(240, $topicable_video->width);
         $this->assertEquals(240, $topicable_video->height);
+        $this->assertEqualsWithDelta(3666, $topicable_video->length, 1);
 
         // pdf
         $this->assertNotNull($topicable_pdf->length);
